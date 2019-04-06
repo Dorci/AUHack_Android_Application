db67715bdd39ec82a5045140ab626c4c2a914294
Prebiusta <davidko123fr@gmail.com>
2019-04-06 12:47:24 +0100

    dasdasd

--- app/src/main/java/com/example/hackathonapp/LoginActivity.java
+++ app/src/main/java/com/example/hackathonapp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.hackathonapp;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -187,6 +188,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
         }
     }
 
